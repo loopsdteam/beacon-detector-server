72cc75efe4f7e204067410899d4a68ca33c39d7c
fkkmemi <fkkmemi@gmail.com>
2019-09-19 08:14:47 +0900

    token react

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import axios from 'axios'
+import moment from 'moment'
 import store from '../store'
 import firebaseConfig from '../../firebaseConfig'
 
@@ -8,8 +9,23 @@ const firebaseAPI = axios.create({
   timeout: 10000
 })
 
-firebaseAPI.interceptors.request.use(function (config) {
+firebaseAPI.interceptors.request.use(async (config) => {
   // Do something before request is sent
+  // console.log(store.state.claims)
+  // console.log(new Date(store.state.claims.exp * 1000))
+  // console.log(new Date(store.state.claims.iat * 1000))
+  // store.state.user.getIdToken()
+  //   .then(r => console.log(r))
+  //   .catch(e => console.error(e.message))
+
+  const dif = moment(store.state.claims.exp * 1000).diff(moment(), 'minutes')
+  if (dif < 10) await store.dispatch('getToken')
+  // .then(r => {
+  // console.log(new Date(store.state.claims.exp * 1000))
+  // console.log(new Date(store.state.claims.iat * 1000))
+  // console.log(m)
+  // })
+  // .catch(e => console.error(e.message))
   config.headers.authorization = store.state.token
   return config
 }, function (error) {
