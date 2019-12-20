0c57128812790c59aa46c7f67db16abbc55785bb
bongjinpark1 <bongjinpark1@gmail.com>
2019-12-20 14:50:10 +0900

    add day model

---
+++ functions/models/days.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+const fields = {
+  beacon: { type: mongoose.Schema.Types.ObjectId, ref: 'Beacon' },
+  count: { type: Number, default: 0 },
+  createdAt: { type: Date, required: true }
+}
+
+const options = {
+  timestamps: {
+    createdAt: false,
+    updatedAt: true
+  }
+}
+
+const schema = new mongoose.Schema(fields, options)
+const Day = mongoose.model('Day', schema)
+
+module.exports = Day
