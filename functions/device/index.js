8da6d4d08436c3b085cc6876919a3ecce0931083
bongjinpark1 <bongjinpark1@gmail.com>
2019-12-20 15:13:40 +0900

    add address and scannerId

--- functions/device/index.js
+++ functions/device/index.js
@@ -59,26 +59,28 @@ app.post('/', async (req, res) => {
     v.name = r.name
     v._beaconId = r._id
 
-    const beacon = r._id
+    const _beaconId = r._id
     const createdAt = moment(v.endTime).startOf('day').add(-9, 'hour').toDate()
+    const address = v.address
 
-    await Day.findOneAndUpdate({ createdAt, beacon }, { $set: { beacon, createdAt }, $inc: { count: v.count } }, { upsert: true, setDefaultsOnInsert: true })
+    await Day.findOneAndUpdate({ createdAt, _beaconId, address }, { $set: { _beaconId, createdAt, address }, $inc: { count: v.count } }, { upsert: true, setDefaultsOnInsert: true })
+      .catch(() => {})
     await BeaconLog.create(v)
 
-    if (v.address === '58:7a:62:02:9b:4f' || v.address === '30:45:11:f0:c2:5b') {
-      try {
-        await db.collection('beaconLogs').add({
-          scanner: data.scanner._id,
-          address: v.address,
-          createdAt: new Date()
-        })
-        const increment = admin.firestore.FieldValue.increment(1)
-        await db.collection('beacons').doc(v.address)
-          .update({ time: new Date(), scanner: data.scanner._id, count: increment })
-      } catch (err) {
-        return res.status(500).send({ message: err.message })
-      }
-    }
+    // if (v.address === '58:7a:62:02:9b:4f' || v.address === '30:45:11:f0:c2:5b') {
+    //   try {
+    //     await db.collection('beaconLogs').add({
+    //       scanner: data.scanner._id,
+    //       address: v.address,
+    //       createdAt: new Date()
+    //     })
+    //     const increment = admin.firestore.FieldValue.increment(1)
+    //     await db.collection('beacons').doc(v.address)
+    //       .update({ time: new Date(), scanner: data.scanner._id, count: increment })
+    //   } catch (err) {
+    //     return res.status(500).send({ message: err.message })
+    //   }
+    // }
   }
 
   res.send(result)
--- functions/models/days.js
+++ functions/models/days.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose')
 
 const fields = {
-  beacon: { type: mongoose.Schema.Types.ObjectId, ref: 'Beacon' },
+  _beaconId: { type: mongoose.Schema.Types.ObjectId, ref: 'Beacon' },
+  address: { type: String, index: true },
   count: { type: Number, default: 0 },
-  createdAt: { type: Date, required: true }
+  createdAt: { type: Date, required: true, index: true },
+  _scannerId: { type: mongoose.Schema.Types.ObjectId, ref: 'Scanner', index: true }
 }
 
 const options = {
@@ -15,7 +17,7 @@ const options = {
 
 const schema = new mongoose.Schema(fields, options)
 
-schema.index({ createdAt: -1, beacon: 1 })
+schema.index({ createdAt: -1, _beaconId: 1, _scannerId: 1 }, { unique: true })
 
 const Day = mongoose.model('Day', schema)
 
