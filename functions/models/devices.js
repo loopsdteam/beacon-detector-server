e3deca0f45d725807b7fefeec63f47b8a01cfee0
fkkmemi <fkkmemi@gmail.com>
2020-01-17 17:03:24 +0900

    device model

---
+++ functions/models/devices.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+const fields = {
+  version: { type: String, default: '0.0.0' },
+  name: { type: String, default: '' },
+  ota: { type: Boolean, defalut: true },
+  cycle: { type: Number, default: 30 },
+  serialNo: { type: String, default: '' },
+  targetURL: { type: String, default: '' },
+  active: { type: Boolean, default: false },
+  inspector: { type: String, default: '' },
+  beaconLength: { type: Number, default: 0 }
+}
+
+const options = {
+  timestamps: true
+}
+
+const schema = new mongoose.Schema(fields, options)
+const Device = mongoose.model('Device', schema)
+
+module.exports = Device
