5c6dbd293f7667117a4114b13ef98ba6d2b00bee
fkkmemi <fkkmemi@gmail.com>
2020-02-11 16:57:56 +0900

    level > 1

--- functions/v2/index.js
+++ functions/v2/index.js
@@ -79,7 +79,7 @@ app.use(require('../middlewares/verifyToken'))
 
 app.use((req, res, next) => {
   if (req.claims.level === undefined) return res.status(403).send({ message: 'not authorized' })
-  if (req.claims.level > 0) return res.status(403).send({ message: 'not authorized' })
+  if (req.claims.level > 1) return res.status(403).send({ message: 'not authorized' })
   next()
 })
 
